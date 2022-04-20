d5937355c401ea1be7c8748fc2fc78d72d049c1f
Alexander Lapin <sascha.lapin2015@yandex.ru>
2022-04-20 10:41:29 +0300

    main

---
+++ Pandas/pandas_temp.py
@@ -0,0 +1,15 @@
+from scipy import stats
+import pandas
+import matplotlib.pyplot as plt
+
+df = pandas.read_csv("temperature.csv")
+
+df.plot(kind='bar')
+df.plot.kde()
+
+print(stats.kstest('norm', 'norm', N=3))
+print(stats.kstest('norm', 'norm', N=500))
+print()
+
+print(stats.kstest(df, 'norm', (df.mean(), df.std()), N=len(df)))
+plt.show()
